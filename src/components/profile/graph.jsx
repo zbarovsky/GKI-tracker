99a955d8a21376407416a2c50ec06a8999f975c2
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-03-04 14:36:22 -0800

    got rough outline for D3 graph running

--- src/components/profile/graph.jsx
+++ src/components/profile/graph.jsx
@@ -1,10 +1,39 @@
 import * as d3 from 'd3';
+import { useEffect, useRef } from 'react';
 
 export const Graph = (props) => {
 
+    const d3Container = useRef(null);
+
+    let mockData = [0,2,4,5,1]
+
+    useEffect(() => {
+        if(d3Container.current) {
+            const svg = d3.select(d3Container.current)
+
+            const update = svg
+                .append('g')
+                .selectAll('text')
+                .data(mockData)
+
+            update.enter()
+                .append('text')
+                .attr('x', (d,i) => i * 25)
+                .attr('y', 40)
+                .style('font-size', 24)
+                .text((d) => d)
+            update 
+                .attr('x', (d,i) => i * 40)
+                .text((d) => d)
+            update.exit()
+                .remove()
+        }
+    }, [mockData, d3Container.current])
+
     return (
         <div>
-            -- d3 graph goes here --
+            <h1>History</h1>
+            <svg className='d3-component' ref={d3Container}/>
         </div>
     )
 }
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -2,11 +2,12 @@ import {Graph} from './graph';
 import {GkiResults} from './gkiResults'
 
 export const Profile = (props) => {
+    //console.log(props.location.state)
 
     return (
         <div>
             <h1>Welcome User</h1>
-            <GkiResults />
+            {/* <GkiResults /> */}
             <Graph />
         </div>
     )
