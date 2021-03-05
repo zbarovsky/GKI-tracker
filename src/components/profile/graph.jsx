077a5707f06b31d9cf3d6bf940967500b346ee26
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-03-05 15:32:33 -0800

    working graph, still has some bugs, but working as should

--- src/components/profile/graph.jsx
+++ src/components/profile/graph.jsx
@@ -1,6 +1,5 @@
 import * as d3 from 'd3';
-import { useEffect, useRef, useState } from 'react';
-import {useLocation} from 'react-router-dom'
+import { useEffect, useRef} from 'react';
 
 export const Graph = (props) => {
 
@@ -9,20 +8,14 @@ export const Graph = (props) => {
     data.push(props.output)
     //console.log(data)
 
-    //const [time, setTime] = useState([])
-    // useEffect(() => {
-    //     let t = new Date().toLocaleTimeString()
-    //     setTime(t)
-    // }, [])
-    
-
     useEffect(() => {
         if(d3Container.current) {
             
             const canvasHeight = 400
             const canvasWidth = 600
 
-            const x = d3.scaleTime().domain([Date.now() - 3 * 60 * 60 * 1000, Date.now() + 12 * 60 * 60 * 1000])
+            const x = d3.scaleTime()
+                .domain([Date.now() - 3 * 60 * 60 * 1000, Date.now() + 12 * 60 * 60 * 1000])
                 .range([0, 800])
         
             const y = d3.scaleLinear()
@@ -40,14 +33,14 @@ export const Graph = (props) => {
                     .attr("fill", "none")
                     .attr("stroke", "#0036E6")
                     .attr("r", 5)
-                    .attr('cx', (datapoint, iteration) => iteration + 20)
+                    .attr('cx', (datapoint, iteration) => x(Date.now()) + 15)
                     .attr("cy", (datapoint) => y(datapoint) - 15)
 
             
             svg.selectAll("text")
                 .data(data).enter()
                     .append("text")
-                    .attr('x', (datapoint, i) => i * 45 + 30)
+                    .attr('x', (datapoint, i) => x(Date.now()) + 15)
                     .attr('y', (datapoint, i) => y(datapoint) - 20)
                     .text(datapoint => datapoint)
 
@@ -59,7 +52,7 @@ export const Graph = (props) => {
                 .attr("transform", "translate(20, -20)")
                 .call(d3.axisLeft(y))
         }
-    }, [props.output, d3Container.current])
+    }, [data, d3Container.current])
 
     return (
         <div>
