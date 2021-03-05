04056c7afdac21ef63a4a1eb33d5564fa52e9384
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-03-04 16:52:03 -0800

    updated spelling errors

--- src/components/profile/explination.jsx
+++ src/components/profile/explination.jsx
@@ -4,13 +4,13 @@ export const Explination = () => {
     return (
         <div className='explination'>
             <div className='information'>
-                <p>The lower the glucose, the lower your overall GKI. This means less inflamation which leads to ailments randing from being overweight to cancer.</p>
+                <p>The lower the glucose, the lower your overall GKI. This means less inflamation which leads to ailments ranging from being overweight to cancer.</p>
                 <p>Your GKI determines the intensity level for nutritional ketosis.</p>
                 <p><b>Keep in mind</b>
                 <br></br>
                 <b>0-1</b> Highest Therapuetic ketosis
                 <br></br>
-                <b>1-3</b> Ketosis necessary for battling certain, cancers, brain ailments, and dementia including Alzheimer's and Parkinson's
+                <b>1-3</b> Ketosis necessary for battling certain cancers, brain ailments, and dementia including Alzheimer's and Parkinson's
                 <br></br>
                 <b>3-6</b> Ketosis necessary for keeping Type 2 Diabetes, insulin resistance and other metabolic syndrome symtpoms at bay
                 <br></br>
